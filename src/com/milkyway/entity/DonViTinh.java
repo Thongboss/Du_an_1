8bbca9f14414a636c7f972ecb559d5d9733ee40a
hungn1ph <84304254+hungn1ph@users.noreply.github.com>
2021-11-06 16:53:07 +0700

    hungupdate

---
+++ src/com/milkyway/entity/DonViTinh.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.milkyway.entity;
+
+/**
+ *
+ * @author ASUS
+ */
+public class DonViTinh {
+   private String MaDVT,TenDVT;
+
+    public String getMaDVT() {
+        return MaDVT;
+    }
+
+    public void setMaDVT(String MaDVT) {
+        this.MaDVT = MaDVT;
+    }
+
+    public String getTenDVT() {
+        return TenDVT;
+    }
+
+    public void setTenDVT(String TenDVT) {
+        this.TenDVT = TenDVT;
+    }
+    
+   
+   
+   
+    
+}
