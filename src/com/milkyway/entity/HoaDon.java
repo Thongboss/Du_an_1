cc512e105c239fb722d6aec04c07c0f2ddc2167d
thaotttph13959 <thaotttph13959@fpt.edu.vn>
2021-11-06 17:07:10 +0700

    Update thucthe

---
+++ src/com/milkyway/entity/HoaDon.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.milkyway.entity;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Admin
+ */
+public class HoaDon {
+    private int MaHD;
+    private Date NgayLap;
+    private int MaNV;
+    private int MaTheTV;
+    private double TongTien;
+    private String GhiChu;
+    private int TrangThai;
+
+    public HoaDon() {
+    }
+
+    public int getMaHD() {
+        return MaHD;
+    }
+
+    public void setMaHD(int MaHD) {
+        this.MaHD = MaHD;
+    }
+
+    public Date getNgayLap() {
+        return NgayLap;
+    }
+
+    public void setNgayLap(Date NgayLap) {
+        this.NgayLap = NgayLap;
+    }
+
+    public int getMaNV() {
+        return MaNV;
+    }
+
+    public void setMaNV(int MaNV) {
+        this.MaNV = MaNV;
+    }
+
+    public int getMaTheTV() {
+        return MaTheTV;
+    }
+
+    public void setMaTheTV(int MaTheTV) {
+        this.MaTheTV = MaTheTV;
+    }
+
+    public double getTongTien() {
+        return TongTien;
+    }
+
+    public void setTongTien(double TongTien) {
+        this.TongTien = TongTien;
+    }
+
+    public String getGhiChu() {
+        return GhiChu;
+    }
+
+    public void setGhiChu(String GhiChu) {
+        this.GhiChu = GhiChu;
+    }
+
+    public int getTrangThai() {
+        return TrangThai;
+    }
+
+    public void setTrangThai(int TrangThai) {
+        this.TrangThai = TrangThai;
+    }
+    
+    
+}
---
+++ src/com/milkyway/entity/TheThanhVien.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.milkyway.entity;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Admin
+ */
+public class TheThanhVien {
+    private int MaTheTV;
+    private String tenKH;
+    private boolean GioiTinh;
+    private Date NgaySinh;
+    private String SDT,CMND,Email,HinhAnh;
+    private Date NgayTao,NgayHetHan;
+    private int NguoiTao;
+
+    public TheThanhVien() {
+    }
+
+    public int getMaTheTV() {
+        return MaTheTV;
+    }
+
+    public void setMaTheTV(int MaTheTV) {
+        this.MaTheTV = MaTheTV;
+    }
+
+    public String getTenKH() {
+        return tenKH;
+    }
+
+    public void setTenKH(String tenKH) {
+        this.tenKH = tenKH;
+    }
+
+    public boolean isGioiTinh() {
+        return GioiTinh;
+    }
+
+    public void setGioiTinh(boolean GioiTinh) {
+        this.GioiTinh = GioiTinh;
+    }
+
+    public Date getNgaySinh() {
+        return NgaySinh;
+    }
+
+    public void setNgaySinh(Date NgaySinh) {
+        this.NgaySinh = NgaySinh;
+    }
+
+    public String getSDT() {
+        return SDT;
+    }
+
+    public void setSDT(String SDT) {
+        this.SDT = SDT;
+    }
+
+    public String getCMND() {
+        return CMND;
+    }
+
+    public void setCMND(String CMND) {
+        this.CMND = CMND;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getHinhAnh() {
+        return HinhAnh;
+    }
+
+    public void setHinhAnh(String HinhAnh) {
+        this.HinhAnh = HinhAnh;
+    }
+
+    public Date getNgayTao() {
+        return NgayTao;
+    }
+
+    public void setNgayTao(Date NgayTao) {
+        this.NgayTao = NgayTao;
+    }
+
+    public Date getNgayHetHan() {
+        return NgayHetHan;
+    }
+
+    public void setNgayHetHan(Date NgayHetHan) {
+        this.NgayHetHan = NgayHetHan;
+    }
+
+    public int getNguoiTao() {
+        return NguoiTao;
+    }
+
+    public void setNguoiTao(int NguoiTao) {
+        this.NguoiTao = NguoiTao;
+    }
+    
+    
+    
+}
